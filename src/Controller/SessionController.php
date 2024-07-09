e4d486b0cc5f1de25b7d9e4e84d508af61a87ef4
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-09 17:10:40 +0200

    updt

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
-        
+        $sessions = $sessionRepository->findAll();
         return $this->render('session/index.html.twig', [
             'sessions' => $sessions,
         ]);
