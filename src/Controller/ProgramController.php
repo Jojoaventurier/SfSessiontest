ac46af37b3229883a0e94e8b10fedbd8efd27a86
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-07-17 11:53:45 +0200

    updt

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Unit;
 use App\Entity\Session;
 use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,5 +25,16 @@ class ProgramController extends AbstractController
             'session' => $session,
             'formAddProgram' => $form
         ]);
+    }
+
+    #[Route('/program/{session}/remove/{unit}', name: 'remove_unit')]
+    public function unregister(Session $session, Unit $unit, EntityManagerInterface $entityManager) {
+
+         $session->removeTrainee($trainee);
+         $trainee->removeSession($session);
+
+         $entityManager->flush();
+
+         return $this->redirectToRoute('show_session', ['id' =>$session->getId()]);
     }
 }
